1dbcd921869f7b2d128d2bdca61e0aa1eace2e51
Robin Roy <robin.rrt123gmail.com>
2021-10-10 16:35:05 +0530

    add boilerplate

---
+++ NFTMinterRinkeby.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-3.0
+
+//pragma solidity ^0.8.0;
+
+//import ERC721URIStorage.sol from OpenZeppelin
+
+//create a contract NFTMINTERRINKEBY and inherit from ERC721URISTORAGE
+contract NFTMinterRinkeby is ERC721URIStorage{
+ 
+//initialize variable for tracking _tokenID
+ 
+// add constructor
+ 
+ //create an event when NFT is minted args(tokenID, indexed NFTOwner, imageURL) TODO
+
+ 
+ //function mintNFT()
+ //TODO
+    //mintNFT
+    // emit event
+ 
+ //function _mintNFT() private
+//TODO
+
+}
